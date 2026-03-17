fb8054e8e805f7afe3d4c8945d0565887cfdb460
Marvin Hermann <marfetto@googlemail.com>
2026-03-17 18:47:48 +0100

    fix: use relative asset/footer paths for deployment

--- JS/loadFooter.js
+++ JS/loadFooter.js
@@ -1,5 +1,5 @@
 // Load footer component
-fetch('/components/footer.html')
+fetch('components/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-placeholder').innerHTML = data;
